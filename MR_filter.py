66519ccdb8e27a7490d88c2d081fbb379d28bb81
Josh Yeung <joshyeung126@gmail.com>
2023-10-07 14:15:21 -0400

    added gui features

--- MR_filter.py
+++
@@ -1,2 +0,0 @@
-def filter(datafile):
-    print("Josh sucks")
--- frontend.py
+++ frontend.py
@@ -1,17 +1,39 @@
 import PySimpleGUI as sg
 
-sg.theme('DarkBlue17')   # Add a touch of color
-# All the stuff inside your window.
-layout = [  [sg.Text('Weather:')],
+sg.theme('LightBlue7')
+
+# Loading Screen
+
+loading = [[sg.Text('Fun Fact')], [sg.Button('Start')]]
+loadingWindow = sg.Window('Loading', loading)
+        
+
+# Home Screen
+
+layout = [  [sg.Button('Refresh')],
+            [sg.Text('Weather:')],
             [sg.Text('Location:')],
             [sg.Button('Water Saftey Level'), sg.Button('Species Nearby')] ]
 
-# Create the Window
+
 window = sg.Window('Prokaryote', layout)
-# Event Loop to process "events" and get the "values" of the inputs
+
+
 while True:
+    loadingEvent, loadingValues = loadingWindow.read()
     event, values = window.read()
-    if event == sg.WIN_CLOSED: # if user closes window or clicks cancel
+    if loadingEvent == sg.WIN_CLOSED:
+        loadingWindow.close()
         break
+    if loadingEvent == 'Start':
+        loadingWindow.close()
+        event, values = window.read()
+        if event == sg.WIN_CLOSED:
+            window.close()
+        if event == 'Water Saftey Level':
+            print('hi')
+        if event == 'Species Nearby':
+            print('hello')
+
+
     
-window.close()
